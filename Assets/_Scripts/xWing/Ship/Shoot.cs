7d235fd009de1514f4a5aadcbacd0d3af9bcb5b4
mikedriessen <mlh.driessen@live.nl>
2019-09-11 16:15:16 +0200

    Finished Day 1
    Added Environment, Models, Shooting and flying.

---
+++ Assets/_Scripts/xWing/Ship/Shoot.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    public Rigidbody bullet;
+    public Transform[] shootPos;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+           
+        
+            for (int i = 0; i < shootPos.Length; i++)
+            { 
+                Rigidbody clone;
+                clone = Instantiate(bullet, shootPos[i].position, transform.rotation);
+                clone.velocity = transform.TransformDirection(Vector3.forward * 1000);
+            }
+
+            
+        }
+    }
+}
